223d4d3956ce3a0cd0226d68dc42f71f5be8270b
alirezafamili <famili.alireza@gmail.com>
2014-09-15 12:30:25 +0430

    add ssl in production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
-SampleApp::Application.configure do  # Settings specified here will take precedence over those in config/application.rb.
+SampleApp::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
