03c004d5e9d1bc821dfc199cdf590fd316b0a255
alirezafamili <famili.alireza@gmail.com>
2014-10-12 10:55:17 +0330

    heroku feed fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,9 @@ class StaticPagesController < ApplicationController
 
   def home
     @micropost = current_user.microposts.build if logged_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    if logged_in?
+    @feed_items = current_user.feed.paginate(page: params[:page]) 
+  end
   end
 
   def help
